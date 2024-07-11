30b2f61feac3b930dadfeab1649b99f0dfa46ed6
Mini <minnnidev@gmail.com>
2024-07-11 18:53:56 +0900

    [Refactor] 커리어넷 open api 통신 위한 url, key 관리 수정

--- TwoHoSun/Application/Constants.swift
+++ TwoHoSun/Application/Constants.swift
@@ -23,3 +23,31 @@ extension Constants {
         static let loginState = "loginState"
     }
 }
+
+extension Constants {
+
+    struct URLConst {
+        static var baseURL: String {
+            guard let baseURL = Bundle.main.infoDictionary?["BASE_URL"] as? String else {
+                fatalError("BASE_URL is missing in the Info.plist")
+            }
+            return baseURL.replacingOccurrences(of: " ", with: "")
+        }
+
+        static var cnetAPI: String {
+            return "http://www.career.go.kr/cnet/openapi/getOpenApi"
+        }
+    }
+}
+
+extension Constants {
+
+    struct APIKeyConst {
+        static var apiKey: String {
+            guard let key = Bundle.main.object(forInfoDictionaryKey: "SCHOOL_API_KEY") as? String else {
+                fatalError("SCHOOL_API_KEY error")
+            }
+            return key
+        }
+    }
+}
